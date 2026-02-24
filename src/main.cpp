38791042043b4b95a0006fb1fe0af842000d7968
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-02-24 19:30:16 +0000

    added up/down functionality

--- src/main.cpp
+++ src/main.cpp
@@ -120,6 +120,12 @@ class Player {
                 sprite.set_x(sprite.x() - speed);
             }
             // TODO: Add logic for up and down
+            if(bn::keypad::up_held()) {
+                sprite.set_y(sprite.y() - speed);
+            }
+            if(bn::keypad::down_held()) {
+                sprite.set_y(sprite.y() + speed);
+            }
 
             bounding_box = create_bounding_box(sprite, size);
         }
